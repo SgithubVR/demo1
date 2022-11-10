b20806ee57a916e60ea56ef0af9ff27363e60047
diepeeters <diepeeters@deloitte.nl>
2022-11-10 14:44:08 +0100

    # changes made by diepeeters2022 after notebook

--- magic.py
+++ magic.py
@@ -1,12 +1,6 @@
-## intro
-
-print("hello world!")
-
-print("Edge is joining!!")
-
 import requests
 import pandas as pd
-
+import matplotlib as plt
 response = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&outputsize=full&apikey=demo")
 
 # Since we are retrieving stuff from a web service, it's a good idea to check for the return status code
@@ -17,5 +11,35 @@ if response.status_code != 200:
 # The service sends JSON data, we parse that into a Python datastructure
 raw_data = response.json()
 
-print(raw_data)
+# So it's a dict. what are the keys?
+print(raw_data.keys())
 
+# Let's look at the first key/value.
+# This is just some descriptive information
+raw_data['Meta Data']
+
+# The other key/value pair is the actual time series.
+# This is a dict as well
+time_series = raw_data['Time Series (5min)']
+type(time_series)
+print(time_series)
+# How many items are in there?
+print(len(time_series))
+
+# Let's take the first few keys
+first_ten_keys = list(time_series.keys())[:10]
+# And see the corresponding values
+first_ten_items = [f"{key}: {time_series[key]}" for key in first_ten_keys ]
+print("\n".join(first_ten_items))
+
+data = raw_data['Time Series (5min)']
+df = pd.DataFrame(data).T.apply(pd.to_numeric)
+df.info()
+
+# Next we parse the index to create a datetimeindex
+df.index = pd.DatetimeIndex(df.index)
+# Let's fix the column names by chopping off the first 3 characters
+df.rename(columns=lambda s: s[3:], inplace=True)
+
+print(df.head())
+df[['open', 'high', 'low', 'close']].plot()

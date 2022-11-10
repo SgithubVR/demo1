58e7067fdf1fee2dc288d6bc3dc718ce1a6e8c89
savanrijn <savanrijn@deloitte.nl>
2022-11-10 15:55:05 +0100

    Final lucky 7(?)

--- Lucky Ticker.py
+++ Lucky Ticker.py
@@ -1,7 +1,20 @@
 ##In this file we want to randomly select 7 'Lucky' Tickers for the lucky coinflip.
 
+##install packages
 import pandas as pd
+
+##import data
 ticker = pd.read_csv('nasdaq_screener_1668088408246.csv')
-ticker
+#print(ticker)
 
 
+##select the tickers
+ticker_symbol = ticker['Symbol']
+##print(ticker_symbol)
+
+##make dataframe
+df = pd.DataFrame(data=ticker_symbol)
+
+##select the random lucky companies
+df_lucky = df.sample(n = 7)
+print(df_lucky)
